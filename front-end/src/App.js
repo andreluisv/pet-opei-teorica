25fc76ce18f1bee0e980b707c9cd167810a94b99
Andre Luis Vasconcelos <alpvj@cin.ufpe.br>
2021-07-26 16:54:01 -0300

    basic form ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [initialState, setInitialState] = useState(0);
+  const [values, setValues] = useState({
+    cpf: "",
+    ra: "",
+  });
+
+  const handleCpfInputChange = (event) => {
+    setValues({...values, cpf: event.target.value});
+  }
+
+  const handleRaInputChange = (event) => {
+    setValues({...values, ra: event.target.value});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="form-container">
+    <form class="register-form">
+      <input
+        onChange={handleRaInputChange}
+        value={values.ra}
+        id="ra-field"
+        class="form-field"
+        type="text"
+        placeholder="RA"
+        name="firstName"
+      />
+      <input
+        onChange={handleCpfInputChange}
+        value={values.cpf}
+        id="cpf-field"
+        class="form-field"
+        type="text"
+        placeholder="CPF"
+        name="lastName"
+      />
+      <button class="form-field" type="submit">
+        Ok
+      </button>
+    </form>
+  </div>
   );
 }
 
