6e67bee385fcdad2b1a63b5522f44d833db5665b
Andre Luis Vasconcelos <alpvj@cin.ufpe.br>
2021-08-26 11:35:45 -0300

    partial side bar and question box

--- web/src/modules/Exam/index.js
+++ web/src/modules/Exam/index.js
@@ -1,11 +1,16 @@
-import { useEffect, useState } from 'react';
 import './Exam.css'
+import { useEffect, useState } from 'react';
+import Logo from '../../assets/logos/opei.svg';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 const Exam = () => {
 
   const [choices, setChoices] = useState([]);
   const [username, setUserName] = useState('');
   const [credentials, setCredentials] = useState([]);
+  const [examname, setExamName] = useState('');
+
+  const [showSideBar, setSideBar] = useState(true);
 
   useEffect(() => {
     const local = JSON.parse(localStorage.getItem('opei-teorica'));
@@ -14,12 +19,30 @@ const Exam = () => {
       setChoices(local.choices);
       setUserName(local.nome);
       setCredentials([atob(local.cpf), local.ra])
+      setExamName(local.prova);
     }
   }, [])
 
   return (
     <div className="container">
-      <h1>Olá, {username} de cpf: {credentials[0]} e ra {credentials[1]}</h1>
+      <div className="sidebar">
+        <button className="menu-icon" onClick={()=>{setSideBar(!showSideBar)}}>
+          <GiHamburgerMenu />
+        </button>
+        <img src={Logo} alt="opei-logo" />
+        <div className="content" style={showSideBar ? null : {display: 'none'}}>
+          <p className="title">{username}</p>
+          <p className="subtitle">{examname}</p>
+          <br/>
+          <p className="title">Tempo restante de prova</p>
+          <p className="subtitle">1:30:54</p>
+          <br/>
+          <p className="title">QUESTÕES</p>
+        </div>
+      </div>
+      <div className="question">
+
+      </div>
     </div>
   );
 }
